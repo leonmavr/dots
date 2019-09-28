657e9e36089fc981af803da6ba4612962a203841
0xleo <0xleo@gmail.com>
2019-09-28 16:41:29 +0200

    screen temepature menu: use smaller font

--- .config/polybar/scripts/screen_temp_rofi.sh
+++ .config/polybar/scripts/screen_temp_rofi.sh
@@ -9,7 +9,7 @@ selected=`echo "6500 (default)
 4000
 3800
 3600
-3400" | rofi -dmenu -p "screen temp" -location 1 -width 16 -lines 5 -yoffset 34 -xoffset 220`
+3400" | rofi -dmenu -p "screen temp" -location 1 -width 16 -lines 5 -font "Roboto Condensed 11" -yoffset 34 -xoffset 220`
 
 temp=`echo $selected | grep -Eo [0-9]*`
 #echo $temp
