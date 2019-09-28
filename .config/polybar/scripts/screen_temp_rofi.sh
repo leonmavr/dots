166b258e2f5d488c61ee93751f579aad94f11321
0xleo <0xleo@gmail.com>
2019-09-28 16:38:37 +0200

    use a rofi-based menu for screen temperature

---
+++ .config/polybar/scripts/screen_temp_rofi.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+selected=`echo "6500 (default)
+6000
+5500
+5000
+4500
+4200
+4000
+3800
+3600
+3400" | rofi -dmenu -p "screen temp" -location 1 -width 16 -lines 5 -yoffset 34 -xoffset 220`
+
+temp=`echo $selected | grep -Eo [0-9]*`
+#echo $temp
+
+if [ ! -z $temp ]; then
+	redshift -x
+	redshift -O $temp
+fi
