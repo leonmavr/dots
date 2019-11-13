72232a098c01516a5ab31441317eb7056771a9c2
0xleo <mvrleo@gmail.com>
2019-11-14 00:41:47 +0100

    up screen temp menu - location & selected tep
    1. move it at the right location depending on number of open workspaces, 2. print a dot next to the last selected value

--- .config/polybar/scripts/screen_temp_rofi.sh
+++ .config/polybar/scripts/screen_temp_rofi.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
-selected=`echo "6500 (default)
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - -  
+n_ws=`i3-msg -t get_workspaces | jq '. | length'`
+# icon width depends on polybar config - find out what it is
+ws_icon_width=26
+ws_width_total=$[ 165+$ws_icon_width*$n_ws ]
+
+current_val=`cat /tmp/redshift_val`
+[ -z $current_val ] && current_val=then_i_will_not_grep_this_value
+
+
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - -  
+selected=`echo "6500
 6000
 5500
 5000
@@ -9,7 +20,15 @@ selected=`echo "6500 (default)
 4000
 3800
 3600
-3400" | rofi -dmenu -p "screen temp" -location 1 -width 16 -lines 5 -font "Roboto Condensed 11" -yoffset 34 -xoffset 220`
+3400
+3200
+3000" | sed -s "s/$current_val/$current_val   ●/g" | rofi -dmenu -p "screen temp"\
+	-location 1\
+	-width 16\
+	-lines 5\
+	-font "Roboto Condensed 11"\
+	-yoffset 34\
+	-xoffset $ws_width_total`
 
 temp=`echo $selected | grep -Eo [0-9]*`
 #echo $temp
@@ -18,4 +37,5 @@ if [ ! -z $temp ]; then
 	redshift -x
 	sleep 1
 	redshift -O $temp
+	echo $temp > /tmp/redshift_val
 fi
