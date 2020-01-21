6aeb47c94d5135ffaff6840615e99cfdf175c365
0xLeo <mvrleo@gmail.com>
2020-01-21 12:20:08 +0100

    wget bash configs in home dir

---
+++ bash_setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget "https://raw.githubusercontent.com/0xLeo/dotfiles/master/.bash_aliases" -O ~/.bash_aliases
+wget "https://raw.githubusercontent.com/0xLeo/dotfiles/master/.bash_history" -O ~/.bash_history
+wget "https://raw.githubusercontent.com/0xLeo/dotfiles/master/.bash_shopt" -O ~/.bash_shopt
+wget "https://raw.githubusercontent.com/0xLeo/dotfiles/master/.bash_prompt" -O ~/.bash_prompt
