002b2220a03c3bc898008cfcd4a36daf242b3ff0
0xleo <0xleo.git@gmail.com>
2020-09-16 17:26:35 +0200

    User abs path when calling emoji python fetcher

--- .config/i3/scripts/emojis.sh
+++ .config/i3/scripts/emojis.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-if [ "$#" -ne 1 ]; then
+if [ "$#" -eq 0 ]; then
 	echo -e "Usage:\n./<this_script> <colon_separated_file>"
 	echo -e "The color-separated file must contain an emoji on the left field of each line and its description on the right."
 	exit 0
 fi
 
-[ ! -f "$1" ] && python3 get_emojis.py "$1" 
+[ ! -f "$1" ] && python3 ~/.config/i3/scripts/get_emojis.py "$1" 
 
 selected=`cat "$1"\
 | rofi -dmenu -p "" -location 3 -width 20 -lines 10 -hide-scrollbar -font "Roboto Condensed 11" -yoffset 34 -xoffset -4`
