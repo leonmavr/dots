3d98da28b3067e3b343863290478d8c6cf25ef6e
leonmavr <0xleo.git@gmail.com>
2021-05-24 17:20:03 +0300

    Move bash-related configs in one folder

--- bash_setup.sh
+++ bash_setup.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-wget "https://raw.githubusercontent.com/0xLeo/dotfiles/master/.bash_aliases" -O ~/.bash_aliases
-wget "https://raw.githubusercontent.com/0xLeo/dotfiles/master/.bash_history_cfg" -O ~/.bash_history_cfg
-wget "https://raw.githubusercontent.com/0xLeo/dotfiles/master/.bash_shopt" -O ~/.bash_shopt
-wget "https://raw.githubusercontent.com/0xLeo/dotfiles/master/.bash_prompt" -O ~/.bash_prompt
-wget "https://raw.githubusercontent.com/0xLeo/dotfiles/master/.inputrc" -O ~/.inputrc
-wget "https://raw.githubusercontent.com/0xLeo/dotfiles/master/.gdbinit" -O ~/.gdbinit
-wget "https://raw.githubusercontent.com/0xLeo/dotfiles/master/.gitconfig" -O ~/.gitconfig
-wget "https://raw.githubusercontent.com/0xLeo/dotfiles/master/.fzf.bash" -O ~/.fzf.bash
+bash_dir=~/.bash
+mkdir $bash_dir
+wget "https://raw.githubusercontent.com/0xLeo/dotfiles/master/.bash_aliases" -O $bash_dir/.bash_aliases
+wget "https://raw.githubusercontent.com/0xLeo/dotfiles/master/.bash_history_cfg" -O $bash_dir/.bash_history_cfg
+wget "https://raw.githubusercontent.com/0xLeo/dotfiles/master/.bash_shopt" -O $bash_dir/.bash_shopt
+wget "https://raw.githubusercontent.com/0xLeo/dotfiles/master/.bash_prompt" -O $bash_dir/.bash_prompt
+wget "https://raw.githubusercontent.com/0xLeo/dotfiles/master/.inputrc" -O $bash_dir/.inputrc
+wget "https://raw.githubusercontent.com/0xLeo/dotfiles/master/.gdbinit" -O $bash_dir/.gdbinit
+wget "https://raw.githubusercontent.com/0xLeo/dotfiles/master/.gitconfig" -O $bash_dir/.gitconfig
+wget "https://raw.githubusercontent.com/0xLeo/dotfiles/master/.fzf.bash" -O $bash_dir/.fzf.bash
 
 for bash in {aliases,history_cfg,shopt,prompt}; do
-	grep -v -q .bash_$bash ~/.bashrc && echo "[ -f ~/.bash_$bash ] && . ~/.bash_$bash" >> ~/.bashrc
+	grep -v -q $bash_dir/.bash_$bash ~/.bashrc && echo "[ -f $bash_dir/.bash_$bash ] && . $bash_dir/.bash_$bash" >> ~/.bashrc
 done
 
 # requirement: fzf package
-if [ -f ~/.fzf.bash ]; then
-	grep -v -q .fzf.bash ~/.bashrc && echo "[ -f ~/.fzf.bash ] && source ~/.fzf.bash" >> ~/.bashrc
+if [ -f $bash_dir/.fzf.bash ]; then
+	grep -v -q $bash_dir/.fzf.bash ~/.bashrc && echo "[ -f $bash_dir/.fzf.bash ] && source $bash_dir/.fzf.bash" >> ~/.bashrc
 fi
