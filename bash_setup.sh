bff562800d358b8d53e9364bf6b7791bf678dc93
Leo Mav <0xLeo@users.noreply.github.com>
2021-12-25 19:00:53 +0200

    Add comments

--- bash_setup.sh
+++ bash_setup.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 bash_dir=~/.bash
-cp -r $bash_dir ~
+cp -r .bash ~
 
+# these scripts should be added to PATH
 if [ -d ~/.bash/scripts ]; then
     toadd="PATH=\${PATH}:~/.bash/scripts"
     grep -v -q "$toadd" ~/.bashrc && echo "# Add my common scripts to the PATH so I don't have spend time sourcing them" >> ~/.bashrc
     grep -v -q "$toadd" ~/.bashrc && echo $toadd >> ~/.bashrc
 fi
 
+# bash configs
 for bash in {aliases,history_cfg,shopt,prompt}; do
 	grep -v -q $bash_dir/.bash_$bash ~/.bashrc && echo "[ -f $bash_dir/bash_$bash ] && . $bash_dir/bash_$bash" >> ~/.bashrc
 done
@@ -17,3 +19,6 @@ done
 if [ -f $bash_dir/.fzf.bash ]; then
 	grep -v -q $bash_dir/.fzf.bash ~/.bashrc && echo "[ -f $bash_dir/.fzf.bash ] && source $bash_dir/.fzf.bash" >> ~/.bashrc
 fi
+
+cp .gitconfig ~
+cp .inputrc ~
