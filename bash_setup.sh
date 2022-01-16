1cdb5cc79930aa49a3366ca51ff70b47493fd69a
leonmavr <0xleo.git@gmail.com>
2022-01-16 20:56:20 +0100

    Add nvim config in setup script

--- bash_setup.sh
+++ bash_setup.sh
@@ -3,6 +3,7 @@
 bash_dir=~/.bash
 cp -r .bash ~
 
+# TODO: make a function that wraps this grep -v -q command
 # these scripts (little tools) should be added to PATH
 if [ -d ~/.bash/scripts ]; then
     toadd="PATH=\${PATH}:~/.bash/scripts"
@@ -22,6 +23,17 @@ fi
 
 git_completion_command="[ -f ~/.git-completion.bash ] && . ~/.git-completion.bash"
 grep -v -q git-completion.bash ~/.bashrc && echo $git_completion_command >> ~/.bashrc
+
+# bash prompt - needs powerline fonts and material icons though
+OS_ICON=
+ps1_string="PS1='\n\[\e[0;38;5;195m\]╭─\[\e[0;38;5;234m\]\[\e[0;38;5;50;48;5;234m\]$OS_ICON \[\e[0;38;5;195;48;5;234m\]\u\[\e[0;38;5;234;48;5;236m\]\[\e[0;38;5;195;48;5;236m\]:: \w\[\e[0;38;5;236;48;5;239m\]\[\e[0;38;5;195;48;5;239m\]: `git rev-parse --abbrev-ref HEAD 2>/dev/null`\[\e[0;38;5;239m\]\[\e[m\]\n\[\e[0;38;5;195m\]╰▻ \[\e[m\]'"
+grep -v -q  ~/.bashrc && echo $ps1_string >> ~/.bashrc
+
+#nvim config
+if [ `which nvim` ]; then
+    mkdir -p ~/.config
+    cp -r .config/nvim ~/.config
+fi
 
 # other configs
 cp .gitconfig ~
