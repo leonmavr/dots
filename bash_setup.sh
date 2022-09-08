4a65f04f0c2c976d2129f5c50032ed1e4c3b54ef
Leo Mav <13514096+leonmavr@users.noreply.github.com>
2022-09-08 22:11:46 +0900

    fix some characters in PS1

--- bash_setup.sh
+++ bash_setup.sh
@@ -1,66 +1,24 @@
-#!/bin/bash
+#---------------------------------------------
+# Requirements:
+# material icons
+#---------------------------------------------
 
-### Requirements:
-# xclip
-
-### Installations
-cd /tmp
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install --all
-cd -
-
-### Configs
-bash_dir=~/.bash
-cp -r .bash ~
-
-# TODO: make a function that wraps this grep -v -q command
-# these scripts (little tools) should be added to PATH
-if [ -d ~/.bash/scripts ]; then
-    toadd="PATH=\${PATH}:~/.bash/scripts"
-    grep -v -q "$toadd" ~/.bashrc && echo "# Add my common scripts to the PATH so I don't have spend time sourcing them" >> ~/.bashrc
-    grep -v -q "$toadd" ~/.bashrc && echo $toadd >> ~/.bashrc
-fi
-
-# bash configs
-for bash in {aliases,history_cfg,shopt,prompt}; do
-	grep -v -q $bash_dir/.bash_$bash ~/.bashrc && echo "[ -f $bash_dir/bash_$bash ] && . $bash_dir/bash_$bash" >> ~/.bashrc
-done
-
-# requirement: fzf package
-if [ -f $bash_dir/.fzf.bash ]; then
-	grep -v -q $bash_dir/.fzf.bash ~/.bashrc && echo "[ -f $bash_dir/.fzf.bash ] && source $bash_dir/.fzf.bash" >> ~/.bashrc
-fi
-
-git_completion_command="[ -f ~/.git-completion.bash ] && . ~/.git-completion.bash"
-grep -v -q git-completion.bash ~/.bashrc && echo $git_completion_command >> ~/.bashrc
-
-# bash prompt - needs powerline fonts and material icons though
 OS_ICON=
-ps1_string="PS1='\n\[\e[0;38;5;195m\]╭─\[\e[0;38;5;234m\]\[\e[0;38;5;50;48;5;234m\]$OS_ICON \[\e[0;38;5;195;48;5;234m\]\u\[\e[0;38;5;234;48;5;236m\]\[\e[0;38;5;195;48;5;236m\]:: \w\[\e[0;38;5;236;48;5;239m\]\[\e[0;38;5;195;48;5;239m\]: `git rev-parse --abbrev-ref HEAD 2>/dev/null`\[\e[0;38;5;239m\]\[\e[m\]\n\[\e[0;38;5;195m\]╰▻ \[\e[m\]'"
-grep -v -q  ~/.bashrc && echo $ps1_string >> ~/.bashrc
 
-# configs
-cp -r .config/* ~
-
-# nvim config
-if [ `which nvim` ]; then
-    mkdir -p ~/.config
-    cp -r .config/nvim ~/.config
-fi
-
-# .local executables such as youtube-dl
-mkdir -p ~/.local/bin
-path_string="PATH=\${PATH}:\$HOME/.local/bin"
-grep -v -q ":\$HOME/.local/bin" ~/.bashrc && echo $path_string >> ~/.bashrc
-
-# other configs
-cp .gitconfig ~
-cp .inputrc ~
-cp .gdbinit ~
-
-# ranger config
-if [ `which ranger` ]; then
-	# see https://github.com/alexanderjeurissen/ranger_devicons
-	git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
-	echo "default_linemode devicons" >> $HOME/.config/ranger/rc.conf
-fi
+chps() {
+        clear
+        if [ ! -f /tmp/bash_prompt_style ]; then
+                # with the aid of https://scriptim.github.io/bash-prompt-generator/
+                PS1='\n\[\e[0;38;5;195m\]╭─\[\e[0;38;5;234m\]\[\e[0;38;5;50;48;5;234m\]$OS_ICON \[\e[0;38;5;195;48;5;234m\]\u\[\e[0;38;5;234;48;5;236m\]\[\e[0;38;5;195;48;5;236m\]: \w\[\e[0;38;5;236;48;5;239m\]\[\e[0;38;5;195;48;5;239m\]: `git rev-parse --abbrev-ref HEAD 2>/dev/null`\[\e[0;38;5;239m\]\[\e[m\]\n\[\e[0;38;5;195m\]╰▻ \[\e[m\]'
+                echo 1 > /tmp/bash_prompt_style
+        elif [ `cat /tmp/bash_prompt_style` -eq 1 ]; then
+                PS1='\[\033[38;5;195m\] \u@\h:\[$(tput bold)\]\w\[$(tput sgr0)\]\[\033[38;5;195m\] :\[$(tput bold)\]`git rev-parse --abbrev-ref HEAD 2>/dev/null`\n⤷ \[$(tput sgr0)\]\[$(tput sgr0)\]'
+                echo 2 > /tmp/bash_prompt_style
+        elif [ `cat /tmp/bash_prompt_style` -eq 2 ]; then
+                PS1="\[$(tput bold)\]\[\033[38;5;195m\] \[$(tput sgr0)\]"
+                echo 3 > /tmp/bash_prompt_style
+        else
+                PS1='\n\[\e[0;38;5;195m\]╭─\[\e[0;38;5;234m\]\[\e[0;38;5;50;48;5;234m\]$OS_ICON \[\e[0;38;5;195;48;5;234m\]\u\[\e[0;38;5;234;48;5;236m\]\[\e[0;38;5;195;48;5;236m\]: \w\[\e[0;38;5;236;48;5;239m\]\[\e[0;38;5;195;48;5;239m\]: `git rev-parse --abbrev-ref HEAD 2>/dev/null`\[\e[0;38;5;239m\]\[\e[m\]\n\[\e[0;38;5;195m\]╰▻ \[\e[m\]'
+                echo 1 > /tmp/bash_prompt_style
+        fi
+}
