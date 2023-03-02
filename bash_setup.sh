c7e2874ceeb901996daa9412f8a9bd344c9a1aa2
leonmavr <0xleo.git@gmail.com>
2023-03-02 21:51:10 +0100

    Fix bash prompt and its setup

--- bash_setup.sh
+++ bash_setup.sh
@@ -2,10 +2,8 @@
 # xclip
 
 ### Installations
-cd /tmp
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --all
-cd -
 
 ### Configs
 bash_dir=~/.bash
@@ -32,11 +30,6 @@ fi
 git_completion_command="[ -f ~/.git-completion.bash ] && . ~/.git-completion.bash"
 grep -v -q git-completion.bash ~/.bashrc && echo $git_completion_command >> ~/.bashrc
 
-# bash prompt - needs powerline fonts and material icons though
-OS_ICON=
-ps1_string="PS1='\n\[\e[0;38;5;195m\]╭─\[\e[0;38;5;234m\]\[\e[0;38;5;50;48;5;234m\]$OS_ICON \[\e[0;38;5;195;48;5;234m\]\u\[\e[0;38;5;234;48;5;236m\]\[\e[0;38;5;195;48;5;236m\]:: \w\[\e[0;38;5;236;48;5;239m\]\[\e[0;38;5;195;48;5;239m\]: `git rev-parse --abbrev-ref HEAD 2>/dev/null`\[\e[0;38;5;239m\]\[\e[m\]\n\[\e[0;38;5;195m\]╰▻ \[\e[m\]'"
-grep -v -q  ~/.bashrc && echo $ps1_string >> ~/.bashrc
-
 # configs
 cp -r .config/* ~
 
@@ -57,8 +50,8 @@ cp .inputrc ~
 cp .gdbinit ~
 
 # ranger config
-if [ `which ranger` ]; then
+#if [ `which ranger` ]; then
 	# see https://github.com/alexanderjeurissen/ranger_devicons
-	git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
-	echo "default_linemode devicons" >> $HOME/.config/ranger/rc.conf
-fi
+	#git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
+	#echo "default_linemode devicons" >> $HOME/.config/ranger/rc.conf
+#fi
