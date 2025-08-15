8e5af4d3fe805c739f3f2de597d780fb4d29af80
leonmavr <0xleo.git@gmail.com>
2025-08-15 13:27:38 +0200

    Update some comments

--- .bash/scripts/add_dir_to_path.sh
+++ .bash/scripts/add_dir_to_path.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 ### About
+# Adds current directory to the PATH. This way, you can use local binary files
+# without adding them to the root, e.g. at /usr/bin.
+#
+### Using it with i3 
 # This script is meant to be sourced to add the current `~/.bash/scripts` directory to the PATH variable.
 # This is because the i3 windows manager ignores changes made to the environment via .bashrc or .xprofile.
 # Therefore every time a keybind that invokes a script in ~/.bash/scripts is activated, the latter directory needs to be manually added to PATH.
